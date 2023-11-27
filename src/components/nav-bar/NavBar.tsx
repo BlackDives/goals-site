47fbbc4ad827099c83e1c91713b01f41f8e76495
BlackDives <sirkamronwilson00@gmail.com>
2023-11-27 14:43:05 -0600

    basic search bar created

--- src/components/nav-bar/NavBar.tsx
+++ src/components/nav-bar/NavBar.tsx
@@ -1,17 +1,64 @@
-import React from "react";
-import { Flex, Text, UnorderedList, ListItem, Link } from "@chakra-ui/react";
+import React from 'react'
+import { Flex, Text, Input, Button } from '@chakra-ui/react'
 
 const NavBar = () => {
   return (
-    <Flex>
-      <Flex>
-        <Text>Goals</Text>
+    <Flex
+      flexDirection='row'
+      justifyContent='space-around'
+      alignItems='center'
+      width='100%'
+      height='100px'
+      backgroundColor={'darkMode.background'}
+    >
+      <Flex
+        height='100%'
+        width='20%'
+        flexDirection='row'
+        alignItems='center'
+        justifyContent='center'
+      >
+        <Text fontSize='50px' color={'darkMode.primary'}>
+          GLZ
+        </Text>
       </Flex>
-      <Flex>
-        <Flex backgroundColor="gray" width="50px" height="50px" />
+      <Flex
+        height='100%'
+        width='80%'
+        flexDirection='row'
+        alignItems='center'
+        justifyContent='flex-start'
+      >
+        <Flex
+          height='50px'
+          width='50%'
+          border='2px solid'
+          borderColor='darkMode.primary'
+          borderRadius={3}
+          marginX={10}
+        >
+          <Input
+            placeholder='Search'
+            height='100%'
+            width='90%'
+            border='none'
+            borderLeftRadius={5}
+          />
+          <Button
+            height='100%'
+            width='10%'
+            border='none'
+            borderRightRadius={0}
+            borderLeftRadius={0}
+            backgroundColor='darkMode.primary'
+            color='darkMode.text'
+          >
+            Search
+          </Button>
+        </Flex>
       </Flex>
     </Flex>
-  );
-};
+  )
+}
 
-export { NavBar };
+export { NavBar }
---
+++ src/components/nav-section/NavSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Flex, Text } from '@chakra-ui/react'
+
+const NavSection = () => {
+  return (
+    <Flex>
+      <Text>Nav Section</Text>
+    </Flex>
+  )
+}
+
+export { NavSection }
